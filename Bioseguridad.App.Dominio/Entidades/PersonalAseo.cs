7c944f20ef29135c96426d89b49ee33e929c46ee
almarav <almarav7@gmail.com>
2021-09-25 15:00:05 -0500

    first commit

---
+++ Bioseguridad.App.Dominio/Entidades/PersonalAseo.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace Bioseguridad.App.Dominio
+{
+    public class PersonalAseo : Persona
+    {
+        public int id {get;set;}
+        public string turnoDesarrolloServicio {get;set;}
+        
+    }
+}
